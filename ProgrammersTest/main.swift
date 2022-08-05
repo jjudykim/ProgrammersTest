73549e73c4b8cd640f5539dc08b3ec974f7e63f2
jjudy_kim <jjudykim0@gmail.com>
2022-08-05 17:34:04 +0900

    12969 done.

--- ProgrammersTest/main.swift
+++ ProgrammersTest/main.swift
@@ -5,7 +5,15 @@
 //  Created by jjudy on 2022/08/05.
 //
 
+//12969
 import Foundation
 
-print("Hello, World!")
+let n = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
+let (a, b) = (n[0], n[1])
 
+for i in 1...b {
+    for j in 1...a {
+        print("*", terminator: "")
+    }
+    print()
+}
