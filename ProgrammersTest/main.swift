943edeebf6b76ee9cdc81ebafba6da9d1231c62a
jjudy_kim <jjudykim0@gmail.com>
2023-01-29 02:19:33 +0900

    <<01.23 ~ test 3/3>> Programmers 120807 done.

--- ProgrammersTest/main.swift
+++ ProgrammersTest/main.swift
@@ -5,9 +5,9 @@
 //  Created by jjudy on 2022/08/05.
 //
 
-//120805
+//120807
 import Foundation
 
 func solution(_ num1:Int, _ num2:Int) -> Int {
-    return num1 / num2
+    return num1 == num2 ? 1 : -1
 }
