f7f905627b10a475aa9760f1dc734d24daa0e364
jjudy_kim <jjudykim0@gmail.com>
2023-02-12 02:50:38 +0900

    <<02.06 ~ test 2/3>> Programmers 120829 done.

--- ProgrammersTest/main.swift
+++ ProgrammersTest/main.swift
@@ -5,9 +5,9 @@
 //  Created by jjudy on 2022/08/05.
 //
 
-//120806
+//120829
 import Foundation
 
-func solution(_ num1:Int, _ num2:Int) -> Int {
-    return Int(Double(num1) / Double(num2) * 1000.0)
+func solution(_ angle:Int) -> Int {
+    return angle < 90 ? 1 : angle == 90 ? 2 : angle < 180 ? 3 : 4
 }
