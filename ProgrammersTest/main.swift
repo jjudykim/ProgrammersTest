52452acfeb5400b274554748e911065d4a57482a
jjudy_kim <jjudykim0@gmail.com>
2023-02-22 10:44:37 +0900

    <<02.20 ~ test 3/7>> Programmers 120833 done.

--- ProgrammersTest/main.swift
+++ ProgrammersTest/main.swift
@@ -5,10 +5,15 @@
 //  Created by jjudy on 2022/08/05.
 //
 
-//120830
+//120833
 
 import Foundation
 
-func solution(_ n:Int, _ k:Int) -> Int {
-    return (n * 12000 + k * 2000) - (n / 10 * 2000)
+func solution(_ numbers:[Int], _ num1:Int, _ num2:Int) -> [Int] {
+    var newNumbers = [Int]()
+    for i in num1...num2 {
+        newNumbers.append(numbers[i])
+    }
+
+    return newNumbers
 }
