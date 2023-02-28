411c4fadd698cc4fa57f48e12d8b0b6d0e0ed0f1
jjudy_kim <jjudykim0@gmail.com>
2023-03-01 00:13:42 +0900

    <<02.27 ~ test 1/7>> Programmers 120847 done.

--- ProgrammersTest/main.swift
+++ ProgrammersTest/main.swift
@@ -5,14 +5,11 @@
 //  Created by jjudy on 2022/08/05.
 //
 
-//120833
+//120847
 
 import Foundation
 
-func solution1(_ numbers:[Int], _ num1:Int, _ num2:Int) -> [Int] {
-    return (num1...num2).map{numbers[$0]}
-}
-
-func solution2(_ numbers:[Int], _ num1:Int, _ num2:Int) -> [Int] {
-    return Array(numbers[num1...num2])
+func solution(_ numbers:[Int]) -> Int {
+    var sortedNumbers = numbers.sorted()
+    return sortedNumbers.suffix(2).reduce(1, *)
 }
