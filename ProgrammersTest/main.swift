731420308ce5112ede25f5a959de315028d5f363
jjudy_kim <jjudykim0@gmail.com>
2023-03-07 23:41:56 +0900

    <<03.06 ~ test 1/7>> Programmers 120822 done.

--- ProgrammersTest/main.swift
+++ ProgrammersTest/main.swift
@@ -5,11 +5,10 @@
 //  Created by jjudy on 2022/08/05.
 //
 
-//120847
+//120822
 
 import Foundation
 
-func solution(_ numbers:[Int]) -> Int {
-    var sortedNumbers = numbers.sorted()
-    return sortedNumbers.suffix(2).reduce(1, *)
+func solution(_ my_string:String) -> String {
+    return String(my_string.reversed())
 }
